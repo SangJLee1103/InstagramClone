041180beb05a4ecf495d7c33032d36c49274f456
sangjun <jesusfollow1103@naver.com>
2022-11-11 15:26:06 +0900

    feat: 로그인 화면, 회원가입 화면 구성

---
+++ InstagramClone/Controllers/Auth/LoginViewController.swift
@@ -0,0 +1,119 @@
+//
+//  LoginViewController.swift
+//  InstagramClone
+//
+//  Created by 이상준 on 2022/11/11.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private var viewModel = LoginViewModel()
+    
+    private let iconImage: UIImageView = {
+        let iv = UIImageView(image: UIImage(named: "Instagram_logo_white"))
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    private let emailTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "이메일")
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+    
+    private let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "비밀번호")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("로그인", for: .normal)
+        button.setTitleColor(.white.withAlphaComponent(0.67), for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.5)
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.isEnabled = false
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "비밀번호를 잊으셨나요", secondPart: "")
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "계정이 없으신가요? ", secondPart: "회원가입")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        configureNotificationObserver()
+    }
+    
+    // MARK: - 액션
+    @objc func handleShowSignUp() {
+        let controller = RegistrationViewController()
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @objc func textDidChange(sender: UITextField) {
+        if sender == emailTextField {
+            viewModel.email = sender.text
+        } else {
+            viewModel.password = sender.text
+        }
+        
+        updateForm()
+    }
+    
+    func configureUI() {
+        configureGradientLayer()
+        
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 80, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, forgotPasswordButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+    }
+    
+    
+    func configureNotificationObserver() {
+        emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+}
+
+// MARK: FormViewModel
+extension LoginViewController: FormViewModel {
+    func updateForm() {
+        loginButton.backgroundColor = viewModel.buttonBackgroundColor
+        loginButton.setTitleColor(viewModel.buttonTitleColor, for: .normal)
+        loginButton.isEnabled = viewModel.formIsValid
+    }
+}
--- InstagramClone/SceneDelegate.swift
+++ InstagramClone/SceneDelegate.swift
@@ -16,7 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = MainTabViewController()
+//        window?.rootViewController = MainTabViewController()
+        window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
         window?.makeKeyAndVisible()
     }
 
