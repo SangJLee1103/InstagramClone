61661f676d5d74872b28a477a2618f3657958e97
이상준 <>
2024-09-25 21:16:16 +0900

    feat: SearchReactor 구현 완료 및 SearchViewController 바인딩 구현 완료

--- InstagramClone/ViewControllers/Search/SearchViewController.swift
+++ InstagramClone/ViewControllers/Search/SearchViewController.swift
@@ -15,9 +15,6 @@ private let reuseIdentifier = "UserCell"
 
 final class SearchViewController: UITableViewController {
     
-    //    private var users = [User]()
-    //    private var filterUsers = [User]()
-    
     private let reactor: SearchReactor
     private let disposeBag = DisposeBag()
     
@@ -39,27 +36,20 @@ final class SearchViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-//        fetchUsers()
         configureSearchController()
         bind(reactor: reactor)
     }
     
-    //    func fetchUsers() {
-    //        UserService.fetchUsers { users in
-    //            self.users = users
-    //            self.tableView.reloadData()
-    //        }
-    //    }
-    
     private func configureTableView() {
         view.backgroundColor = .white
         
+        tableView.delegate = nil
+        tableView.dataSource = nil
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
     }
     
     private func configureSearchController() {
-//        searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.searchBar.placeholder = "검색"
@@ -94,36 +84,3 @@ final class SearchViewController: UITableViewController {
             .disposed(by: disposeBag)
     }
 }
-
-// MARK: 테이블 뷰 데이터소스
-//extension SearchViewController {
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return inSearchMode ? filterUsers.count : users.count
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-//        
-//        let user = inSearchMode ? filterUsers[indexPath.row] : users[indexPath.row]
-//        cell.viewModel = UserCellViewModel(user: user)
-//        return cell
-//    }
-//}
-
-//extension SearchViewController {
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let user = inSearchMode ? filterUsers[indexPath.row] : users[indexPath.row]
-//        let controller = ProfileViewController(user: user)
-//        navigationController?.pushViewController(controller, animated: true)
-//    }
-//    
-//}
-//
-//
-//extension SearchViewController: UISearchResultsUpdating {
-//    func updateSearchResults(for searchController: UISearchController) {
-//        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
-//        filterUsers = users.filter { $0.username.contains(searchText) || $0.fullname.lowercased().contains(searchText) }
-//        self.tableView.reloadData()
-//    }
-//}
